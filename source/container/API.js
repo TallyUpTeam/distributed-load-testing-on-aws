fc063e1d77eac5f40138018de0256b19d5d7fd2d
Paul Ashdown <paul@tallyup.com>
2021-02-05 15:49:30 -0800

    Updated ErrCode

--- source/container/API.js
+++ source/container/API.js
@@ -5,44 +5,65 @@ import { Utils } from './Utils.js';
 const logger = new Logger('API');
 
 export const ErrCode = {
-	None:                       0,
-	PhoneNumberInvalid:         1,
-	UsernameInvalid:            2,
-	UsernameInUse:              3,
-	UpdateFailure:              4,
-	UserNotFound:               5,
-	PercentageInvalid:          6,
-	AccountInsufficient:        7,
-	CognitoError:               8,
-	PhoneNumberUnconfirmed:     9,
-	AlreadyRegistered:          10,
-	InviteCodeInvalid:          11,
-	AlreadyPlaying:             12,
-	NotAdmitted:                13,
-	NotPlaying:                 14,
-	AdmissionExpired:           15,
-	NotQueued:                  16,
-	AuthError:                  17,
-	CashOutInvalidState:        18,
-	IncompatibleClientVersion:  19,
-	IncompatibleServerVersion:  20,
-	PayeeInvalid:               21,
-    InternalError:              22,
-	InvalidQueryParams:         23,
-	AmountInvalid:              24,
-	GameNotPlayable:            25,
-	AdError:                    26,
-	UserIneligible:             27,
-	TransactionTimeout:         28,
-	DocumentTimeout:            29,
-	CommitTransactionRetriesExceeded: 30,
-	TransactionRetriesExceeded: 31,
-	DocumentRetriesExceeded:    32,
-    OCCVersionMismatch:         33,
-    NoDeviceId:                 34,
-    MultiDevicePlay:            35,
-	InvalidLeaderboardType:     36,
-	InvalidLevelRequest:        37
+	None:                           0,
+	PhoneNumberInvalid:             1,  // Phone number not found in Cognito account (shouldn't be possible)
+	UsernameInvalid:                2,  // Given username undefined, null, or otherwise invalid (too long or short or invalid characters)
+	UsernameInUse:                  3,  // Username already taken, during activation
+	UpdateFailure:                  4,  // Internal error - couldn't update a user in DB after multiple attempts
+	UserNotFound:                   5,  // A user corresponding to the authenticated Cognito account doesn't exist (not registered)
+	PercentageInvalid:              6,  // Charity percentage too low or high, during cashout
+	AccountInsufficient:            7,  // Not enough money, during cashout
+	CognitoError:                   8,  // Cannot find Cognito account for given access token
+	PhoneNumberUnconfirmed:         9,  // Cognito account has unconfirmed phone number (shouldn't be possible)
+	AlreadyRegistered:              10, // This user account already exists, during registration
+	InviteCodeInvalid:              11, // From a deprecated feature but leave here to maintain numeric values
+	AlreadyPlaying:                 12, // This user account already active, during activation
+	NotAdmitted:                    13, // Queued user not admitted yet, during activation
+	NotPlaying:                     14, // User hasn't activated yet
+	AdmissionExpired:               15, // Admission from queue has expired, during activation
+	NotQueued:                      16, // Attempt to get queue rank of user not in queue
+	AuthError:                      17, // Access token was undefined, missing, or invalid (could not authenticate with it)
+	CashOutInvalidState:            18, // Attempt to cash out during a live game
+	IncompatibleClientVersion:      19, // Client version is too old for server
+	IncompatibleServerVersion:      20, // Server version is too old for client
+	PayeeInvalid:                   21, // Payee information undefined, null, or not a valid phone number or email, during cashout
+	InternalError:                  22, // Internal server error (generic code for HTTP '500 Internal Server Error')
+	InvalidQueryParams:             23, // Missing query (or body) param - only used for missing deviceToken in push note registration
+	AmountInvalid:                  24, // Charity, player, or inviter amount was undefined or null, during cashout
+	GameNotPlayable:                25, // Game initialized but not in a playable state yet (unused)
+	AdError:                        26, // General error during watching an ad or awarding a fresh penny (deprecated)
+	UserIneligible:                 27, // User has been banned
+	TransactionTimeout:             28, // Internal error - multi-update database transaction didn't complete in time
+	DocumentTimeout:                29, // Internal error - single document database update didn't complete in time
+	CommitTransactionRetriesExceeded: 30,   // Internal error - multi-update database transaction couldn't be committed due to conflicting updates
+	TransactionRetriesExceeded:     31, // Internal error - multi-update database transaction couldn't be completed due to conflicting updates
+	DocumentRetriesExceeded:        32, // Internal error - single document database update couldn't be completed due to conflicting updates
+	OCCVersionMismatch:             33, // An internal error used by the system
+	NoDeviceId:                     34, // Device id was undefined or null
+	MultiDevicePlay:                35, // User account is also playing on another device
+	InvalidLeaderboardType:         36, // Requested type undefined, null, or un-recognized
+	InvalidLevelRequest:            37, // General error when requesting a live game
+	InvalidPowerUp:                 38, // Given powerup undefined, null, or unknown type, during tower start
+	NoPowerUpAvailable:             39, // User has no powerups for jump, during tower start
+	NoPenniesAvailable:             40, // User has no free pennies available to claim, during tower start
+	InvalidInviter:                 41, // Inviter user account not found, during set-inviter
+	InvalidQuantity:                42, // Only used by admin UI
+	LevelTooHigh:                   43, // User at too high level to use tower jump, or already has a balance when claiming penny
+	AlreadyInvited:                 44, // User already called set-inviter
+	GlobalPennyCapExceeded:         45, // Cannot claim a new penny because too many already given out today
+	SessionNotFound:                46, // No session found for given sessionId, or no user session corresponds to given gameId
+	OpponentIneligible:             47, // Opponent has been banned, when issuing a challenge
+	OpponentNotPlaying:             48, // Opponent not activated, when issuing a challenge
+	DuplicateChallenge:             49, // User already has a challenge with given opponent, when issuing a challenge
+	InvalidSession:                 50, // User's or opponent's session is in wrong state, when accepting/declining/cancelling/acknowledging a challenge
+	OpponentNotFound:               51, // Opponent username not found, when issuing/accepting/declining/cancelling/acknowledging a challenge
+	NoSessionId:                    52, // Given sessionId was undefined or null
+	NoGameForSession:               53, // Couldn't find corresponding game for session when acknowledging a result
+	OpponentUsernameInvalid:        54, // Given opponent username was undefined, null, or invalid
+	NotAtLevel:                     55, // User doesn't have enough balance for level when issuing or accepting challenge
+	AmountMismatch:                 56, // The client's player, charity, or inviter amounts are incorrect, during cashout end (maybe balance changed?)
+	GameUnavailable:                57, // User hasn't unlocked requested game when issuing or accepting challenge
+	OpponentGameUnavailable:        58  // Opponent hasn't unlocked requested game (or opponent doesn't have any of same games unlocked), when issuing challenge
     // NOTE: Keep this in sync with tallyup-server/src/shared/errors/ErrCode.ts!
 };
 
