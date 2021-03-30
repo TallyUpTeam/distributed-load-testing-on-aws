17535139553b8a400cff23e7e2ac8b7458065ed5
Paul Ashdown <paul@tallyup.com>
2021-03-29 18:14:02 -0700

    Alpine removed 'python' - use 'python3' instead

--- source/container/load-test.sh
+++ source/container/load-test.sh
@@ -12,7 +12,7 @@ aws s3 cp s3://$S3_BUCKET/test-scenarios/$TEST_ID.json /k6-tests/config.json
 echo "Running test"
 k6 run --out json=/k6-tests/out.json --quiet --no-summary --logformat raw /k6-tests/script.js
 
-t=$(python -c "import random;print(random.randint(1, 30))")
+t=$(python3 -c "import random;print(random.randint(1, 30))")
 echo "sleep for: $t seconds."
 sleep $t
 
